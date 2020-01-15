dfc723296cb99041c51b4a3711745dda65df369f
Yogesh Ratudi <yogesh.ratudi@greatplacetowork.com>
2020-01-15 14:07:47 +0530

    Product collection resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,13 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name'=>$this->name,
+            'price' =>$this->price,
+            'totalprice'=>round((1-($this->discount/100))*($this->price),2),
+            'rating'=>$this->reviews->count () > 0 ? 
+                     round($this->reviews->sum('star')/$this->reviews->count (),2) :'No reting yet', 
+                     'disocunt'=>$this->discount,
+             'href'=>['link' =>route('products.show',$this->id)]        ];
     }
 }
